35dc69f206663a197275de79c1ca2a9f989b5e2e
Eddie Shin <eshinhw@gmail.com>
2022-04-25 23:19:16 -0400

    activities and lifecycle

--- app/src/main/java/com/eddieshin/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/eddieshin/androidfundamentals/MainActivity.kt
@@ -4,10 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    // MainActivity inherits from AppCompatActivity()
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) // R represents resources (res folder)
     }
 }

1a918dbce93a291d23f8fa7fe11064f016408eda
Eddie Shin <eshinhw@gmail.com>
2022-04-27 15:10:35 -0400

    button import id

--- app/src/main/java/com/eddieshin/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/eddieshin/androidfundamentals/MainActivity.kt
@@ -2,11 +2,24 @@ package com.eddieshin.androidfundamentals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import kotlinx.android.synthetic.main.practice_constraint_layout.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.practice_linearlayout) // R represents resources (res folder)
+        // setContentView(R.layout.practice_linearlayout) // R represents resources (res folder)
+        setContentView(R.layout.practice_constraint_layout)
+
+        val btnApply = findViewById<Button>(R.id.btnApply)
+        btnApply.setOnClickListener {
+            val firstName = etFirstName.text.toString()
+            val lastName = etLastName.text.toString()
+            val birthDate = etBirthDate.text.toString()
+            val country = etCountry.text.toString()
+            Log.d("MainActivity", "$firstName $lastName was born on $birthDate in $country")
+        }
     }
 }

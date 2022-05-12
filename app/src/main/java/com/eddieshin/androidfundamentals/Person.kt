2ddbeca43f14a3ae4e0a6ee8b296b937c916e305
Eddie Shin <eshinhw@gmail.com>
2022-05-12 13:28:48 -0400

    rearranged files and directories

--- app/src/main/java/com/eddieshin/androidfundamentals/Person.kt
+++
@@ -1,9 +0,0 @@
-package com.eddieshin.androidfundamentals
-
-import java.io.Serializable
-
-data class Person (
-    val name: String,
-    val age: Int,
-    val origin: String
-    ) : Serializable
